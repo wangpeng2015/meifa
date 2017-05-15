b535407c4ccbd5279e659dd96f7b5fedf49bc167
wangpeng2015 <954233685@qq.com>
2017-05-15 14:13:14 +0800

    Add files via upload

---
+++ user_t.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.38 : Database - meifa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`meifa` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `meifa`;
+
+/*Table structure for table `user_t` */
+
+DROP TABLE IF EXISTS `user_t`;
+
+CREATE TABLE `user_t` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `userName` varchar(20) DEFAULT NULL COMMENT '用户名',
+  `passWord` varchar(20) DEFAULT NULL COMMENT '密码',
+  `roles` varchar(20) DEFAULT '0' COMMENT '角色  0代表客户，1代表店主',
+  `email` varchar(20) DEFAULT NULL COMMENT '电子邮件',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_t` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
