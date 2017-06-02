9c1d364691434d1d54e1b812fab1fd8c35341d74
wangpeng2015 <Peng112890>
2017-06-03 06:40:34 +0800

    1.0

--- src/test/java/com/spring/mybatics/serviceTest/TestCustomerMybatis.java
+++ src/test/java/com/spring/mybatics/serviceTest/TestCustomerMybatis.java
@@ -11,7 +11,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.spring.mybatics.dao.ICustomerDao;
+import com.haier.result.ServiceResult;
+import com.spring.mybatics.dao.ICustomerDAO;
+import com.spring.mybatics.domain.Hair_customers;
+import com.spring.mybatics.service.ICustomerService;
 
 /**
  * 
@@ -29,10 +32,54 @@ import com.spring.mybatics.dao.ICustomerDao;
 @Transactional
 public class TestCustomerMybatis {
 	
-	
 	@Resource
-	ICustomerDao iCustomerDao;
+	ICustomerService iCustomerService;
+	@Resource
+	ICustomerDAO iCustomerDao;
 	
+	
+	/**
+	 * 
+	 * 保存新的客户
+	 */
+	@Test
+	@Rollback(false)
+	public void testSaveCustomer(){
+		Hair_customers customer=new Hair_customers();
+		customer.setCustomer_name("消费者1");
+		customer.setCustomer_isVip("1");//是vip  1级别
+		customer.setCustomer_address("山东青岛李沧区");
+		customer.setCustomer_code("cu_001");
+		customer.setCustomer_money(new BigDecimal(100));
+		customer.setCustomer_phoneNumber("18300247761");
+		customer.setCustomer_storeCode("001");
+		iCustomerService.saveCustomer(customer);
+	}
+	
+	/**
+	 * 根据门店查询用户列表
+	 * 
+	 */
+	@Test
+	public void testSearchCustomerByStoreCode(){
+		String storeCode="001";
+		ServiceResult list= iCustomerService.searchCustomerByStoreCode(storeCode);
+		System.out.println(list.getResult().toString());
+	}
+	
+	/**
+	 * 根据电话信息删除用户
+	 */
+	@Test
+	public void testdeleteCustomerByphoneNumber(){
+		String phonenumber="18300247760";
+		ServiceResult res=iCustomerService.deleteCustomerByphoneService(phonenumber);
+		System.out.println(res);
+	}
+	
+	/**
+	 * 根据用户电话更新用户金额
+	 */
 	@Test
 	@Rollback(false) //不回滚
 	public void testupdateCustomer(){
@@ -49,12 +96,21 @@ public class TestCustomerMybatis {
 	}
 	
 	/**
-	 * 根据电话查询客户
+	 * 根据电话更新用户
 	 */
-//	@Test
-	/*public void testupdatephoneNumberByID(){
-		BigDecimal money=iCustomerDao.findCustomerMoneyByPhoneNumber("18300247760");
-		System.out.println(money);
-	}*/
+	@Test
+	@Rollback(false)
+	public void testupdatephoneNumber(){
+		Hair_customers customer=new Hair_customers();
+//		customer.setCustomer_code("002");//设置用户的编码
+//		customer.setUpdateTime(new Date());
+//		customer.setCustomer_integral(new BigDecimal(300));
+//		customer.setCustomer_isVip("1");
+		customer.setCustomer_phoneNumber("18300247760");
+		customer.setCustomer_storeCode("002");//设置门店编码
+		customer.setCustomer_name("张三三");
+		ServiceResult money=iCustomerService.updateCustomerByphoneService(customer);
+		System.out.println(money.getResult().toString());
+	}
 
 }
--- src/test/java/com/spring/mybatics/serviceTest/TestEmployeeDaoMybatis.java
+++ src/test/java/com/spring/mybatics/serviceTest/TestEmployeeDaoMybatis.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.spring.mybatics.dao.IEmployeeDao;
+import com.spring.mybatics.dao.IEmployeeDanDao;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)////表示继承了SpringJUnit4ClassRunner类
@@ -26,7 +26,7 @@ public class TestEmployeeDaoMybatis {
 	
 	
 	@Resource
-	IEmployeeDao iEmployeeDao;
+	IEmployeeDanDao iEmployeeDanDao;
 	
 	/**
 	 * 保存店员单据
@@ -35,7 +35,7 @@ public class TestEmployeeDaoMybatis {
 	@Test
 	@Rollback(false) //不回滚
 	public void testSaveCustomer(){
-		int res=iEmployeeDao.saveEmployeeDanDao(3,2,new BigDecimal(20));
+		int res=iEmployeeDanDao.saveEmployeeDanDao(3,2,new BigDecimal(20));
 		System.out.println(res);
 	} 
 
