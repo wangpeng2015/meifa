89495d5ab0717b38a18f3d7850b413d04f118094
wangpeng2015 <Peng112890>
2017-06-03 06:34:19 +0800

    1.0

--- src/main/java/com/spring/mybatics/dao/ICustomerDao.java
+++ src/main/java/com/spring/mybatics/dao/ICustomerDao.java
@@ -1,12 +1,13 @@
 package com.spring.mybatics.dao;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
 import com.spring.mybatics.domain.Hair_customers;
 
-public interface ICustomerDao  extends CommonDAO<Hair_customers,Integer>{
+public interface ICustomerDAO  extends CommonDAO<Hair_customers,Integer>{
 	
 	/**
 	 * 根据电话更新客户金钱
@@ -21,5 +22,26 @@ public interface ICustomerDao  extends CommonDAO<Hair_customers,Integer>{
 	 * 
 	 */
 	BigDecimal findCustomerMoneyByPhoneNumber(@Param("customer_phoneNumber")String customer_phoneNumber);
+
+	/**
+	 * 根据门店编码查询门店下的客户信息
+	 * @param storeCode
+	 * @return
+	 */
+	List<Hair_customers> searchCustomerListByStoreCode(@Param("storeCode")String storeCode);
+
+	/**
+	 * 根据电话删除用户信息
+	 * @param phoneNumber
+	 * @return
+	 */
+	int deleteCustomerByphoneDao(@Param("phoneNumber")String phoneNumber);
+	
+	/**
+	 * 根据用户电话删除用户信息
+	 * @param phoneNumber
+	 * @return
+	 */
+	int updateCustomerByPhone(Hair_customers customers);
 
 }
--- src/main/java/com/spring/mybatics/dao/IEmployeeDao.java
+++ src/main/java/com/spring/mybatics/dao/IEmployeeDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.spring.mybatics.domain.Hair_employee_dan;
 
-public interface IEmployeeDao extends  CommonDAO<Hair_employee_dan,Integer>{
+public interface IEmployeeDanDao extends  CommonDAO<Hair_employee_dan,Integer>{
 
 	int saveEmployeeDanDao(@Param("danju_applicant_id")int danju_applicant_id,@Param("danju_id")int danju_id,@Param("danju_price")BigDecimal danju_price);
 
